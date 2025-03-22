7070d59da0ab5abf8c9b06848c43e7929303df80
SantiVarona <santiagov0306@gmail.com>
2025-03-21 20:15:40 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efecto de desplazamiento suave para los enlaces
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
