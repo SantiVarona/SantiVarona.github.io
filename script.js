a08ae22e2fefa17ea2e96f4a28af9bab627f6bca
SantiVarona <santiagov0306@gmail.com>
2025-05-04 15:30:11 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Efecto de desplazamiento suave para los enlaces
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
+
